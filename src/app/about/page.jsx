07ecf7d89523777e4c7a9ff84e9794206b4f0f39
Jossy Benitez <jossybr94@gmail.com>
2024-04-08 20:16:21 -0500

    Creando about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <section className="container mx-auto">
+      <h3 className="text 4xl font-bold my-5">About</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
+        nesciunt non fugiat quos facilis voluptatum eaque, ipsam distinctio quia
+        vel. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dignissimos officia atque ducimus ipsa non molestias, rerum modi
+        architecto iure quam numquam expedita est quia officiis ea corrupti
+        necessitatibus nemo suscipit excepturi eius sapiente, quos provident.
+        Animi minima quia dolores officia delectus perferendis incidunt? Tempora
+        cum sit obcaecati natus harum eveniet.
+      </p>
+    </section>
+  );
+}
+
+export default AboutPage;
