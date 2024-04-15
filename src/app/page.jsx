df225760d04218b68e6c27af62a15e5527ea12bd
Jossy Benitez <jossybr94@gmail.com>
2024-04-15 16:43:14 -0500

    Creando los pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { prisma } from "@/libs/prisma";
+import TaskCard from "@/components/TaskCard";
 
-function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+async function loadTasks() {
+  //Obteniendo de la base de datos
+  return await prisma.task.findMany();
+  // console.log(tasks);
+
+  //Haciendo una petición HTTP /api/tasks
+  //   const res = await fetch("http://localhost:3000/api/task");
+  //   const data = await res.json();
+  //   console.log(data);
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTasks();
+  // console.log(tasks);
+
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default HomePage;
